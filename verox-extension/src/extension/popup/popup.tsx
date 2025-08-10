5d73e31106499bb2d717632a5faf81b96d948794
Shaurya Srivastava <shaurya2k06@gmail.com>
2025-08-10 18:06:46 +0530

    Feat: Initialised File Structure for Web Extension

--- verox-extension/src/extension/popup/popup.tsx
+++ verox-extension/src/extension/popup/popup.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+wimport React, { useState } from 'react';
 import { createRoot } from 'react-dom/client';
 import '../../index.css';
 import { registerCredential, authenticateCredential } from './webauthn';
