3be5c7735af21a6a3156deb7917520a4fa095fdd
Shaurya Srivastava <shaurya2k06@gmail.com>
2025-08-10 12:47:24 +0530

    Refactor: Added Biometric stub; needs fixes later

--- verox/src/biometric.rs
+++ verox/src/biometric.rs
@@ -1,4 +1,32 @@
-pub fn verify_biometric() -> bool {
-    println!("Verifying...");
-    true
+use std::io::{self,Write};
+pub struct BiometricAuth {
+    pub fun register() -> io::Result<()> {
+        println!("Registerign Biometric");
+        print!("Set a secure Passphrase");
+        io::stdout().flush()?;
+        let mut passphrase= String::new();
+        io::stdin().read_line(&mut passphrase)?;
+        let passphrase = passphrase.trim();
+
+        std::fs::write(".biometric_key", passphrase)?;
+        println!("Biometric registered");
+        Ok(())
+    }
+    pub fn verify() -> io::Result<bool> {
+        println!("Verifying Biomentric...");
+        print!("Enter Passphrase:");
+        io::stdout().flush()?;
+        let mut input - String::new();
+        io::stdin().read_line(&mut input)?;
+        let input = input.trim();
+        let stored = std::ds::read_to_string(".biometric_key")?.trim().to_string();
+        if input == stored {
+            println!("Biometric Verified.");
+            OK(true)
+
+        } else {
+            println!("Verification failed");
+            Ok(false)
+        }
+    }
 }
